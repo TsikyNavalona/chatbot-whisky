05601e79778f544e8030bc0aa7336f3daff9c871
TsikyNavalona <tsikywatib@gmail.com>
2022-02-13 14:35:46 +0300

    9e commit

--- src/controllers/chatBotController.js
+++ src/controllers/chatBotController.js
@@ -121,12 +121,12 @@ function handlePostback(sender_psid, received_postback) {
   let payload = received_postback.payload;
 
   // Set the response based on the postback payload
-  if (payload === 'yes') {
-    response = { "text": "Thanks!" }
-  } else if (payload === 'no') {
-    response = { "text": "Oops, try sending another image." }
-  }else if (payload === 'GET_STARTED') {
-    response = { "text": "Bienvenue sur le chatBot de Whisky Mada Corp" }
+  switch(payload){
+    case "GET_STARTED":
+      response = { "text": "Bienvenue sur le chatBot de Whisky Mada Corp"};
+      break;
+    default :
+      console.log("erreur");
   }
   // Send the message to acknowledge the postback
   callSendAPI(sender_psid, response);
